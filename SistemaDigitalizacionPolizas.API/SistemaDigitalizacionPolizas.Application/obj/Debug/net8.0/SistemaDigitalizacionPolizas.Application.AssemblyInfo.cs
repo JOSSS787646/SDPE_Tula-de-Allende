26189997b69c4eec1cdfcf50bf05d3e55594f0af
Jose Antonio Martinez <joseantoni787646@gmail.com>
2026-04-07 13:37:58 -0600

    Sprint 12: Implemementacion de DockerFile con mejoras

--- SistemaDigitalizacionPolizas.API/SistemaDigitalizacionPolizas.Application/obj/Debug/net8.0/SistemaDigitalizacionPolizas.Application.AssemblyInfo.cs
+++ SistemaDigitalizacionPolizas.API/SistemaDigitalizacionPolizas.Application/obj/Debug/net8.0/SistemaDigitalizacionPolizas.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDigitalizacionPolizas.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da5b417b993548918eccb05b811c7c0c60f9872e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef5d802a689da6738c12abdefb8c158b1fdc1f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDigitalizacionPolizas.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDigitalizacionPolizas.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
