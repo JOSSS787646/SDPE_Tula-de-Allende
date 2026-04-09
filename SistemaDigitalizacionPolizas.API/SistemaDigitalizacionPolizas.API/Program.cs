6de49937e794208ae234952622e3e378c62c267a
Jose Antonio Martinez <joseantoni787646@gmail.com>
2026-04-09 13:30:54 -0600

    Modificacion para integracion con docker

--- SistemaDigitalizacionPolizas.API/SistemaDigitalizacionPolizas.API/Program.cs
+++ SistemaDigitalizacionPolizas.API/SistemaDigitalizacionPolizas.API/Program.cs
@@ -15,9 +15,9 @@ using SistemaDigitalizacionPolizas.API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-/*
-var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");*/
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 // ======================================================
 // CONTROLLERS
